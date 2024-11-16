32458787d2df93eb0ccf48078518248af19acdcf
miguelkjesus <miguelkjesus05@gmail.com>
2024-11-16 22:36:22 +0000

    Fix auth with session

--- middleware/requestHandlers.js
+++ middleware/requestHandlers.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.js";
 export function authWithSession(requiresLoginCheck) {
   return (req, res, next) => {
     auth0.requiresAuth(requiresLoginCheck)(req, res, async () => {
-      req.session.user = await User.findOne({ email: req.oidc.user.email });
+      req.session.userInfo = await User.findOne({ email: req.oidc.user.email });
       next();
     });
   };
