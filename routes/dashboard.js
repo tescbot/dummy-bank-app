9d16186dfcd5357580d231baec685799872796a5
miguelkjesus <miguelkjesus05@gmail.com>
2024-11-16 21:53:43 +0000

    DONE DASHBOARD WOOOOOOOOOOOOOOOOOOOO

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,17 +3,36 @@ import auth0 from "express-openid-connect";
 
 import { Account } from "../models/account.js";
 import { User } from "../models/user.js";
+import { Transaction } from "../models/transaction.js";
 
 export const path = "/dashboard";
 export const router = express.Router();
 
 router.get("/", auth0.requiresAuth(), async (req, res) => {
   const user = await User.findOne({ email: req.oidc.user.email });
-  const accounts = await Account.find(); //{ userId: user._id });
+  const accounts = await Account.find({ userId: user._id });
+
+  let transactions = {};
+  for (const acc of accounts) {
+    const ts = await Transaction.find({
+      $or: [{ senderAccountId: acc._id }, { recipientAccountId: acc._id }],
+    });
+
+    transactions[acc._id] = [];
+    for (const t of ts) {
+      transactions[acc._id].push({
+        sender: await Account.findById(t.senderAccountId),
+        recipient: await Account.findById(t.recipientAccountId),
+        amountSent: 400,
+        reference: t.reference,
+      });
+    }
+  }
 
   res.render("dashboard", {
     title: "Dashboard",
     user,
     accounts,
+    transactions,
   });
 });
