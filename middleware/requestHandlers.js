2aaf77f9d0cd6471233b07a3c3226a25317eb76d
miguelkjesus <miguelkjesus05@gmail.com>
2024-11-17 11:54:39 +0000

    Last minute bug fixes

--- middleware/requestHandlers.js
+++ middleware/requestHandlers.js
@@ -4,8 +4,13 @@ import { User } from "../models/user.js";
 export function authWithSession(requiresLoginCheck) {
   return (req, res, next) => {
     auth0.requiresAuth(requiresLoginCheck)(req, res, async () => {
-      req.session.userInfo = await User.findOne({ email: req.oidc.user.email });
-      next();
+      const user = await User.findOne({ email: req.oidc.user.email });
+      if (user) {
+        req.session.userInfo = user;
+        next();
+      } else {
+        res.redirect("/user-set-up");
+      }
     });
   };
 }
--- routes/userSetUp.js
+++ routes/userSetUp.js
@@ -1,56 +1,54 @@
 import express from "express";
 import { User } from "../models/user.js";
 import { Account } from "../models/account.js";
-import { validate } from "../middleware/backupValidate.js"
+import { validate } from "../middleware/backupValidate.js";
+import auth0 from "express-openid-connect";
 
 export const path = "/user-set-up";
 export const router = express.Router();
 
-router.get("/", (req, res) => {
-    if(!req.oidc.isAuthenticated()){
-        res.redirect("/login")
-    }else{
-        res.render("userSetUp", {title: "User Set Up", errors: {}});
-    }
+router.get("/", auth0.requiresAuth(), (req, res) => {
+  res.render("userSetUp", { title: "User Set Up", errors: {} });
 });
 
-router.post("/", async (req, res) => {
-    if(!req.oidc.isAuthenticated()){
-        res.redirect("/login")
-    }
-    let fullName = req.body.fullName;
-    let email = req.session.userInfo.email;
-    let dateOfBirth = req.body.dateOfBirth;
-    let phoneNumber = req.body.phoneNumber;
-    let address = {
-        line1: req.body.addressLine1,
-        line2: req.body.addressLine2,
-        city: req.body.city,
-        county: req.body.county,
-        postCode: req.body.postCode,
-        country: req.body.country
-    }
-    let errors = await validate({}, "phoneNumber", phoneNumber, -1);
-    if (Object.keys(errors).length !== 0) {
-        res.render("userSetUp", {title: "User Set Up", errors: errors, bd: req.body});
-        return;
-    }
-
-    let newUser = await User.create({
-        fullName: fullName,
-        email: email,
-        dateOfBirth: dateOfBirth,
-        phoneNumber: phoneNumber,
-        address: address
+router.post("/", auth0.requiresAuth(), async (req, res) => {
+  let fullName = req.body.fullName;
+  let email = req.oidc.user.email;
+  let dateOfBirth = req.body.dateOfBirth;
+  let phoneNumber = req.body.phoneNumber;
+  let address = {
+    line1: req.body.addressLine1,
+    line2: req.body.addressLine2,
+    city: req.body.city,
+    county: req.body.county,
+    postCode: req.body.postCode,
+    country: req.body.country,
+  };
+  let errors = await validate({}, "phoneNumber", phoneNumber, -1);
+  if (Object.keys(errors).length !== 0) {
+    res.render("userSetUp", {
+      title: "User Set Up",
+      errors: errors,
+      bd: req.body,
     });
-    console.log(newUser);
-    req.session.userInfo = newUser;
+    return;
+  }
 
-    // create account
-    let newAccount = await Account.create({
-        userId: req.session.userInfo._id,
-        name: "Current"
-    })
-    req.session.userAccount = newAccount;
-    res.redirect("/dashboard")
+  let newUser = await User.create({
+    fullName: fullName,
+    email: email,
+    dateOfBirth: dateOfBirth,
+    phoneNumber: phoneNumber,
+    address: address,
+  });
+  console.log(newUser);
+  req.session.userInfo = newUser;
+
+  // create account
+  let newAccount = await Account.create({
+    userId: req.session.userInfo._id,
+    name: "Current",
+  });
+  req.session.userAccount = newAccount;
+  res.redirect("/dashboard");
 });
